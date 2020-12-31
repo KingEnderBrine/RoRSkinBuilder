f7292e3f37247e1717925c10382bf3728ddeb876
KingEnderBrine <31794213+KingEnderBrine@users.noreply.github.com>
2020-12-31 14:47:53 +0700

    Fixed `ProjetileGhostReplacement` added ability to load ghost grom game files

---
+++ Editor/CodeGeneration/CustomEditors/ProjectileGhostReplacementPropertyDrawer.cs
@@ -0,0 +1,10 @@
+﻿using RoRSkinBuilder.Data;
+using UnityEditor;
+
+namespace RoRSkinBuilder.CustomEditors
+{
+    [CustomPropertyDrawer(typeof(ProjectileGhostReplacement))]
+    public class ProjectileGhostReplacementPropertyDrawer : HideablePropertyDrawer<ProjectileGhostReplacement>
+    {
+    }
+}
--- Editor/CodeGeneration/Data/ProjectileGhostReplacement.cs
+++ Editor/CodeGeneration/Data/ProjectileGhostReplacement.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RoRSkinBuilder.CustomEditors;
+using System;
 using UnityEngine;
 
 namespace RoRSkinBuilder.Data
@@ -8,7 +9,16 @@ namespace RoRSkinBuilder.Data
     {
         [Tooltip("Path to the projectile GameObject")]
         public string projectilePath;
+        [Tooltip("Load projectile ghost that exists in the game files")]
+        public bool useResourcesPath;
         [Tooltip("New projectile ghost")]
+        [ShowWhen(nameof(useResourcesPath), false, false)]
         public GameObject projectileGhost;
+        [Tooltip("Path to the projectile ghost")]
+        [ShowWhen(nameof(useResourcesPath), false, true)]
+        public string projectileGhostPath;
+
+        [HideInInspector]
+        public bool isExpanded;
     }
 }
