de11107bc936ddda4add36d85069c6a833bf9ec3
KingEnderBrine <kingenderbrin@gmail.com>
2023-08-14 13:41:58 +0700

    2.3.0

--- Editor/CodeGeneration/Data/GameObjectActivation.cs
+++ Editor/CodeGeneration/Data/GameObjectActivation.cs
@@ -1,11 +1,22 @@
 ﻿using System;
+using RoRSkinBuilder.CustomEditors;
+using UnityEngine;
 
 namespace RoRSkinBuilder.Data
 {
     [Serializable]
     public class GameObjectActivation
     {
+        public GameObjectActivationAccessType accessType;
+        [ShowWhen(nameof(accessType), false, GameObjectActivationAccessType.ByRendererIndex)]
         public int rendererIndex;
+        [ShowWhen(nameof(accessType), false, GameObjectActivationAccessType.ByRendererName)]
+        public string rendererName;
+        [ShowWhen(nameof(accessType), false, GameObjectActivationAccessType.ByPath)]
+        public string path;
         public bool shouldActivate;
+
+        [HideInInspector]
+        public bool isExpanded;
     }
 }
